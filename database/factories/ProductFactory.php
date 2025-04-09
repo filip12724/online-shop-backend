a3d33c891e16441e582bdfe200d068c8ed933651
Filip <bondzulicfilip@gmail.com>
2025-04-09 22:05:57 +0200

    Made a api controller for products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,12 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            'discount' => $this->faker->optional()->numberBetween(0, 100),
+            'image' => $this->faker->optional()->imageUrl(640, 480, 'products', true),
+            'stock' => $this->faker->numberBetween(1, 100),
         ];
     }
 }
